b1250cf7b426cdd108a32caf46ef89ec6818f3e7
Cecilia Clohisy <cclohisy@gmail.com>
2018-05-23 11:51:22 -0500

    initialze materialize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,39 +3,48 @@ import { Link } from "react-router-dom";
 
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 const Navbar = props => (
-    <div>
+  <div>
     <nav>
-    <div className="nav-wrapper red accent-2">
-    <Link className="brand-logo center" to="/">
-       Cecilia Clohisy
+      <div className="nav-wrapper red accent-2">
+        <Link className="brand-logo center" to="/">
+          Cecilia Clohisy
        </Link>
-       <Link to ="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i>
-       </Link>
-      {/* <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a> */}
-      <ul className="right hide-on-med-and-down">
-        <li>
-            <Link to ="/about">
-            About 
+        {/* <Link to ="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i>
+       </Link> */}
+        <a href="#" data-target="mobileMenu" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+        <ul className="right hide-on-med-and-down">
+          <li>
+            <Link to="/about">
+              About
             </Link>
 
-        </li>
-        <li>
-        <Link to="/portfolio">
+          </li>
+          <li>
+            <Link to="/portfolio">
               Portfolio
             </Link>
-            
-            </li>
 
-      </ul>
-    </div>
-  </nav>
+          </li>
 
-  <ul className="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
-  </ul>
+        </ul>
+      </div>
+    </nav>
+
+    <ul className="sidenav" id="mobileMenu">
+      <li>
+        <Link to="/portfolio">
+          Portfolio
+            </Link>
+      </li>
+      <li>
+        <Link to="/about">
+          About
+            </Link></li>
+      {/* <li><a href="collapsible.html">contact link</a></li> */}
+      <li><Link className="brand-logo center" to="/">
+          Cecilia Clohisy
+       </Link></li>
+    </ul>
   </div>
 );
 
