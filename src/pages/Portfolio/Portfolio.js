0f16fd098cf4fd0c3a2c696f8e8eec0bdd6beb48
Cecilia Clohisy <cclohisy@gmail.com>
2018-05-23 11:38:49 -0500

    port pg formatting

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -4,13 +4,26 @@ import Footer from "../../components/Footer"
 import ProjectCard from "../../components/ProjectCard"
 import ContactLink from "../../components/ContactLink"
 import "./Portfolio.css"
+import portCards from "../../portData.json"
+
 
 const Portfolio = () => (
     <div>
         <Navbar />
         <div className ="container">
         <div className ="row">
-        <ProjectCard/>
+        {portCards.map(card =>(
+            <ProjectCard
+            key = {card.id}
+            id = {card.id}
+            title = {card.title}
+            link = {card.webLink}
+            description = {card.description}
+            src = {card.src}
+            />
+
+        ))}
+        
         </div>
         <div className = "row">
         <ContactLink/>
